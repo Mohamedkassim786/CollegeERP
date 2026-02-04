6b42d7e3d84ae27d59aeb496e419a7f306246f46
jagadeesh877 <jaga543424@gmail.com>
2026-02-04 15:59:48 +0530

    pefectly fixed first year and all working

--- client/src/pages/admin/StudentManager.jsx
+++ client/src/pages/admin/StudentManager.jsx
@@ -369,7 +369,7 @@ const StudentManager = () => {
                                         disabled={newStudent.year === '1'}
                                     >
                                         <option value="">{newStudent.year === '1' ? 'N/A (First Year)' : 'Select Dept'}</option>
-                                        {departments.filter(d => d.name !== 'First Year (General)').map(d => <option key={d.id} value={d.code || d.name}>{d.code || d.name}</option>)}
+                                        {departments.filter(d => d.name !== 'First Year (General)').map(d => <option key={d.id} value={d.name}>{d.code || d.name}</option>)}
                                     </select>
                                 </div>
                                 <div>
@@ -472,7 +472,7 @@ const StudentManager = () => {
                                         disabled={parseInt(editingStudent.year) === 1}
                                     >
                                         <option value="">{parseInt(editingStudent.year) === 1 ? 'N/A (First Year)' : 'Select Dept'}</option>
-                                        {departments.filter(d => d.name !== 'First Year (General)').map(d => <option key={d.id} value={d.code || d.name}>{d.code || d.name}</option>)}
+                                        {departments.filter(d => d.name !== 'First Year (General)').map(d => <option key={d.id} value={d.name}>{d.code || d.name}</option>)}
                                     </select>
                                 </div>
                                 <div>
